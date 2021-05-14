82fbd59b15c5684743e568399c5f06e19f22405c
saru <saru.gmesp@gmail.com>
2021-05-14 16:44:40 +0200

    Changes done in fullname.js

--- javascript/javascript1/week2/fullname.js
+++ javascript/javascript1/week2/fullname.js
@@ -1,15 +1,15 @@
 function getFullname(firstName, surName, useFormalName, gender) {
-
-  if (useFormalName, gender === "male") {
-    return "Lord" + " " + firstName + " " + surName;
-  } else if (useFormalName, gender === "female") {
-    return "Lady" + " " + firstName + " " + surName;
-  } else {
-    return firstName + " " + surName;
+  if (useFormalName) {
+    if (gender === "male") {
+      return "Lord" + " " + firstName + " " + surName;
+    }
+    if (gender === "female") {
+      return "Lady" + " " + firstName + " " + surName;
+    }
   }
+  return firstName + " " + surName;
 }
 
-
 console.log(getFullname("Benjamin", "Hughes", true, "male"));
-console.log(getFullname("Saru", "Latha", false, "female"));
-console.log(getFullname("Mila", "Andrew"));
+console.log(getFullname("Saru", "Latha", true, "female"));
+console.log(getFullname("Mila", "Andrew", true));
