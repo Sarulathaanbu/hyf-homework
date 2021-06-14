4fe8030f30a6db4e7168debd9bba80b1d58979d2
saru <saru.gmesp@gmail.com>
2021-06-14 16:32:17 +0200

    hyf-bay folder added

---
+++ javascript/javascript2/week2/hyf-bay/main.js
@@ -0,0 +1,90 @@
+console.log('Script loaded');
+
+const products = getAvailableProducts();
+const productsUl = document.querySelector('section.products ul');
+console.log(productsUl);
+
+function renderProducts(products) {
+  products.forEach(product => {
+    const li = document.createElement('li');
+
+    let shipsToHTML = '';
+    product.shipsTo.forEach(country => shipsToHTML += `<li>${country}</li>`);
+
+    li.innerHTML =
+      `
+            <ul>
+                <li>${product.name}</li>
+                <li>${product.price}</li>
+                <li>${product.rating}</li>
+                <ul class="ships-to">${shipsToHTML}</ul>
+            </ul>
+        `;
+    productsUl.appendChild(li);
+  });
+}
+
+renderProducts(products);
+
+const userInput = document.getElementById('search-box');
+const userPrice = document.getElementById('start');
+
+const sortName = document.getElementById('sortname');
+
+
+//   Search Box /////////////////////
+userInput.addEventListener("input", function(event) {
+  const productName = event.target.value;
+  console.log(productName);
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(productName.toLowerCase())
+  );
+  if (productName) {
+    return renderProducts(filteredProducts);
+  }
+  return renderProducts(products);
+});
+///////Cheap Products//////////////////////////////
+
+userPrice.addEventListener("click", function(event) {
+  const userRange = event.target.value;
+  const cheapProducts = products.filter(
+    (product) => product.price <= userRange * 1000
+  );
+  return renderProducts(cheapProducts);
+});
+
+////sort options/////////////////////////////
+sortName.addEventListener('change', () => {
+  const value = sortName.value;
+  if (value === 'desc') {
+    renderProducts(products.sort((firstProduct, secondProduct) => {
+      let firstProductName = firstProduct.name.toLowerCase(),
+        secondProductName = secondProduct.name.toLowerCase();
+
+      if (firstProductName > secondProductName) {
+        return -1;
+      }
+      if (firstProductName < secondProductName) {
+        return 1;
+      }
+      return 0;
+
+    }));
+  } else if (value === 'asc') {
+    renderProducts(products.sort((firstProduct, secondProduct) => {
+      let firstProductName = firstProduct.name.toLowerCase(),
+        secondProductName = secondProduct.name.toLowerCase();
+
+      if (firstProductName < secondProductName) {
+        return -1;
+      }
+      if (firstProductName > secondProductName) {
+        return 1;
+      }
+      return 0;
+
+    }));
+
+  }
+});
