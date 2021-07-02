fd20dc83f6d9c4aab17e72b7693902b7f06b9d69
saru <saru.gmesp@gmail.com>
2021-07-02 11:24:27 +0200

    Feedback Changes done q

--- javascript/javascript3/week1/giphy/giphy.js
+++ javascript/javascript3/week1/giphy/giphy.js
@@ -4,7 +4,6 @@ const getAlert = document.getElementById("alert");
 
 function getGif() {
   const resultDiv = document.createElement("div");
-  resultDiv.innerHTML = "";
   if (!gifSearchName.value) {
     getAlert.innerHTML = "Please enter word to search Gif!!";
 
@@ -16,7 +15,7 @@ function getGif() {
         `https://api.giphy.com/v1/gifs/search?api_key=GBqVaF0BqJQ6cFmXKetzAu1SgA2HlFrA&q=${gifSearchName.value }&limit=${numOfGifs.value}&offset=0&rating=g&lang=en`
       )
       .then(response => response.json())
-      .then(api => (api.data.map(item => {
+      .then(json => (json.data.map(item => {
         const gifImage = document.createElement("img");
         gifImage.src = item.images.original.url;
         resultDiv.appendChild(gifImage);
--- javascript/javascript3/week1/weather/weather.js
+++ javascript/javascript3/week1/weather/weather.js
@@ -5,10 +5,10 @@ const weather = document.querySelector(".weather")
 button.addEventListener('click', () => {
   if (city.value === "") {
     msg.innerHTML = 'Please enter the city';
-    return false;
+
   } else {
     const cityName = city.value;
-    url =
+    const url =
       `https://api.openweathermap.org/data/2.5/weather?q=${cityName} &appid=19a22c19916855a97938c4084c52be58`;
     fetch(url)
       .then(response => response.json())
@@ -17,11 +17,16 @@ button.addEventListener('click', () => {
 });
 
 function renderCityWeatherInfo(data) {
-
-  weather.innerHTML =
-    ` Temperature: ${Math.floor(data.main.temp - 273)} F ${data.weather[0].description} <BR/>
-            Icon for the weather type: <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png"> <BR/>
-            Wind speed: ${data.wind.speed} <BR/>
-            How clowdy it is: ${data.clouds.all}%<BR/>
-            sunrise is: ${data.sys.sunrise} and sunset is: ${data.sys.sunset}`
+  const cityName = city.value;
+  document.querySelector('.city').innerText = `Weather in ${cityName}`;
+  document.querySelector('.icon').innerHTML =
+    `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png">`;
+  document.querySelector('.temp').innerText =
+    `${Math.floor(data.main.temp - 273)} F ${data.weather[0].description}°C`;
+  document.querySelector('.wind').innerHTML =
+    `Wind Speed: ${data.wind.speed} m/s`;
+  document.querySelector('.humidity').innerHTML =
+    `Humidity: ${data.clouds.all}%`;
+  document.querySelector('.sunrise').innerHTML = `Sunrise: ${data.sys.sunrise}`;
+  document.querySelector('.sunset').innerHTML = `Sunset: ${data.sys.sunset}`;
 }
